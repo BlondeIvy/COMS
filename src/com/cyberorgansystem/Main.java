bb1dc6b754c44957d65f5d8c23b801c25c8dc58a
BlondeIvy <135213942+BlondeIvy@users.noreply.github.com>
2024-09-03 21:13:32 -0500

    COMS #1 :09/03/24

---
+++ src/com/cyberorgansystem/Main.java
@@ -0,0 +1,9 @@
+package com.cyberorgansystem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to The Cybernetic Organ Management System!");
+    }
+}
+
+
